dfcfc9ba4108b6f86c25eb0e42680b063540f4a4
John Yoder <johngilesyoder@gmail.com>
2017-06-01 02:18:59 -0600

    Fix blog styles

--- wp-content/themes/glo/single.php
+++ wp-content/themes/glo/single.php
@@ -7,26 +7,35 @@
 
 get_header(); ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
+
 <div class="page-title">
-	<div class="container">
-		<h2>Blog</h2>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<a href="/blog" class="back-to-blog">&larr; Back to blog</a>
+				<h1><?php the_title(); ?></h1>
+			</div>
+		</div>
 	</div>
 </div>
 
 <main class="page-wrapper">
-	<div class="container">
-		<div class="page-content">
-		<?php while ( have_posts() ) : the_post(); ?>
-		
-			<h1 class="post-title"><?php the_title(); ?></h1>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="page-content">
 
-			<?php the_content(); ?>
-				
-			<?php comments_template(); ?> 
+					<?php the_content(); ?>
 
-		<?php endwhile; // End of the loop. ?>
+					<?php comments_template(); ?>
+
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
+
+<?php endwhile; // End of the loop. ?>
 
 <?php get_footer(); ?>
