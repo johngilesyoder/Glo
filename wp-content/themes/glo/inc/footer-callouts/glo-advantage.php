1fdeb85de98334697ec013a137ec5fdaeac50885
John Yoder <johngilesyoder@gmail.com>
2017-06-18 16:59:44 -0600

    Footer callouts responsive

--- wp-content/themes/glo/inc/footer-callouts/glo-advantage.php
+++ wp-content/themes/glo/inc/footer-callouts/glo-advantage.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <h3 class="callout-title">The Glo Advantage</h3>
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-sm-6 col-md-3">
         <div class="advantage">
           <h4>Full Lifetime Warranty</h4>
           <div class="advantage-illustration">
@@ -14,7 +14,7 @@
           <a href="#" class="btn btn-text">View Warranty Info</a>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-sm-6 col-md-3">
         <div class="advantage">
           <h4>Partners in Your Project</h4>
           <div class="advantage-illustration">
@@ -26,7 +26,7 @@
           <a href="#" class="btn btn-text">Planning Tools &amp; Tips</a>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-sm-6 col-md-3">
         <div class="advantage">
           <h4>Energy Efficiency</h4>
           <div class="advantage-illustration">
@@ -38,7 +38,7 @@
           <a href="#" class="btn btn-text">Learn More About Energy Efficiency</a>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-sm-6 col-md-3">
         <div class="advantage">
           <h4>Bespoke Installation</h4>
           <div class="advantage-illustration">
--- wp-content/themes/glo/inc/footer-callouts/request-quote.php
+++ wp-content/themes/glo/inc/footer-callouts/request-quote.php
@@ -1,6 +1,8 @@
 <div class="footer-callout footer-callout-quote">
-  <p>
-    <?php the_field('quote_request_callout_copy', 'option'); ?>
-  </p>
-  <a href="/request-a-quote" class="btn btn-outline">Request a quote</a>
+  <div class="container-fluid">
+    <p>
+      <?php the_field('quote_request_callout_copy', 'option'); ?>
+    </p>
+    <a href="/request-a-quote" class="btn btn-outline">Request a quote</a>
+  </div>
 </div>
