8ff773b7110ce630a15cd5a71e07086b4e0ce8ba
John Yoder <johngilesyoder@gmail.com>
2017-06-23 03:15:15 -0600

    fix popover

--- wp-content/themes/glo/inc/product/r-value.php
+++ wp-content/themes/glo/inc/product/r-value.php
@@ -4,6 +4,6 @@
   <div class="r-value-wrapper">
     <h3 class="r-value-title">R-Value</h3>
     <span class="r-value-value"><?php the_field('r-value'); ?></span>
-    <a tabindex="0" role="button" class="btn-r-value-info" data-toggle="popover" data-container="body" data-trigger="focus" data-placement="right" title="What is R-Value?" data-content="Insulation levels are specified by R-Value. R-Value is a measure of insulation’s ability to resist heat traveling through it. The higher the R-Value the better the thermal performance of the insulation."><i class="zmdi zmdi-info"></i></a>
+    <a tabindex="0" role="button" class="btn-r-value-info" data-toggle="popover" data-container="body" data-trigger="click" data-placement="left" title="What is R-Value?" data-content="Insulation levels are specified by R-Value. R-Value is a measure of insulation’s ability to resist heat traveling through it. The higher the R-Value the better the thermal performance of the insulation."><i class="zmdi zmdi-info"></i></a>
   </div>
 </div>
