c1d3960369bac0135185d0e7f07ddc5f48c499aa
John Yoder <johngilesyoder@gmail.com>
2017-07-23 18:53:44 -0600

    Try to fix blog image loading sizes

--- wp-content/themes/glo/inc/blog-tile.php
+++ wp-content/themes/glo/inc/blog-tile.php
@@ -1,7 +1,7 @@
-<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+<?php $url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' ); ?>
 <article class="col-sm-6 col-md-4">
   <a class="blog-post" href="<?php the_permalink(); ?>">
-    <div class="post-image" style="background-image:url('<?php echo $url; ?>');"></div>
+    <div class="post-image" style="background-image:url('<?php echo $url[0]; ?>');"></div>
     <h1 class="post-title"><?php the_title(); ?></h1>
     <span class="read-more">Read More &nbsp;&rarr;</span>
   </a>
