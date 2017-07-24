bf94643b383870236b9f0116980cf79ab5cf01f9
John Yoder <johngilesyoder@gmail.com>
2017-07-23 18:25:42 -0600

    Fix available product styles on product page

--- wp-content/themes/glo/inc/product/available-styles.php
+++ wp-content/themes/glo/inc/product/available-styles.php
@@ -1,12 +1,4 @@
 <?php
-  $terms_materials = apply_filters( 'taxonomy-images-get-terms', '', array(
-    'taxonomy'       => 'frame-material',
-    'having_images'  => false,
-    'term_args'      => array(
-      'taxonomy'       => 'frame-material',
-      'hide_empty'     => false
-    )
-  ) );
   if (has_term( 'windows', 'product-line' )) {
     $terms = get_the_terms( $post->ID, 'window-style' );
   } elseif (has_term( 'doors', 'product-line' )) {
@@ -25,6 +17,7 @@
     <ul class="operating-styles">
       <?php foreach ($terms as $term) { ?>
         <?php
+        if (has_term( 'windows', 'product-line' )) {
           $styles = apply_filters( 'taxonomy-images-get-terms', '', array(
             'taxonomy' => 'window-style',
               'term_args' => array(
@@ -32,6 +25,15 @@
               )
             )
           );
+        } elseif (has_term( 'doors', 'product-line' )) {
+          $styles = apply_filters( 'taxonomy-images-get-terms', '', array(
+            'taxonomy' => 'door-style',
+              'term_args' => array(
+                'slug' => $term->slug,
+              )
+            )
+          );
+        }
         ?>
         <?php foreach( (array) $styles as $style) { ?>
           <li class="menu-item">
