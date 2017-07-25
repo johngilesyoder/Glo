9fd966f1c00f661b20632c0ec57576ae3f7c5078
John Yoder <johngilesyoder@gmail.com>
2017-07-25 12:08:29 -0600

    Fix admin js issue / Fix callout images

--- wp-content/themes/glo/functions.php
+++ wp-content/themes/glo/functions.php
@@ -498,7 +498,7 @@ class CT_TAX_META_MATERIAL {
    add_action( 'created_frame-material', array ( $this, 'save_material_image' ), 10, 2 );
    add_action( 'frame-material_edit_form_fields', array ( $this, 'update_material_image' ), 10, 2 );
    add_action( 'edited_frame-material', array ( $this, 'updated_material_image' ), 10, 2 );
-   add_action( 'admin_footer', array ( $this, 'add_script' ) );
+   //add_action( 'admin_footer', array ( $this, 'add_script' ) );
  }
 
  /*
@@ -568,55 +568,6 @@ class CT_TAX_META_MATERIAL {
    }
  }
 
-/*
- * Add script
- * @since 1.0.0
- */
- public function add_script() { ?>
-   <script>
-     jQuery(document).ready( function($) {
-       function ct_media_upload(button_class) {
-         var _custom_media = true,
-         _orig_send_attachment = wp.media.editor.send.attachment;
-         $('body').on('click', button_class, function(e) {
-           var button_id = '#'+$(this).attr('id');
-           var send_attachment_bkp = wp.media.editor.send.attachment;
-           var button = $(button_id);
-           _custom_media = true;
-           wp.media.editor.send.attachment = function(props, attachment){
-             if ( _custom_media ) {
-               $('#category-image-id').val(attachment.id);
-               $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-               $('#category-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
-             } else {
-               return _orig_send_attachment.apply( button_id, [props, attachment] );
-             }
-            }
-         wp.media.editor.open(button);
-         return false;
-       });
-     }
-     ct_media_upload('.ct_tax_media_button.button');
-     $('body').on('click','.ct_tax_media_remove',function(){
-       $('#category-image-id').val('');
-       $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-     });
-     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
-     $(document).ajaxComplete(function(event, xhr, settings) {
-       var queryStringArr = settings.data.split('&');
-       if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
-         var xml = xhr.responseXML;
-         $response = $(xml).find('term_id').text();
-         if($response!=""){
-           // Clear the thumb image
-           $('#category-image-wrapper').html('');
-         }
-       }
-     });
-   });
- </script>
- <?php }
-
   }
 
 $CT_TAX_META_MATERIAL = new CT_TAX_META_MATERIAL();
@@ -647,7 +598,7 @@ class CT_TAX_META_STYLE {
    add_action( 'created_window-style', array ( $this, 'save_windowstyle_image' ), 10, 2 );
    add_action( 'window-style_edit_form_fields', array ( $this, 'update_windowstyle_image' ), 10, 2 );
    add_action( 'edited_window-style', array ( $this, 'updated_windowstyle_image' ), 10, 2 );
-   add_action( 'admin_footer', array ( $this, 'add_script' ) );
+   //add_action( 'admin_footer', array ( $this, 'add_script' ) );
  }
 
  /*
@@ -717,55 +668,6 @@ class CT_TAX_META_STYLE {
    }
  }
 
-/*
- * Add script
- * @since 1.0.0
- */
- public function add_script() { ?>
-   <script>
-     jQuery(document).ready( function($) {
-       function ct_media_upload(button_class) {
-         var _custom_media = true,
-         _orig_send_attachment = wp.media.editor.send.attachment;
-         $('body').on('click', button_class, function(e) {
-           var button_id = '#'+$(this).attr('id');
-           var send_attachment_bkp = wp.media.editor.send.attachment;
-           var button = $(button_id);
-           _custom_media = true;
-           wp.media.editor.send.attachment = function(props, attachment){
-             if ( _custom_media ) {
-               $('#category-image-id').val(attachment.id);
-               $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-               $('#category-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
-             } else {
-               return _orig_send_attachment.apply( button_id, [props, attachment] );
-             }
-            }
-         wp.media.editor.open(button);
-         return false;
-       });
-     }
-     ct_media_upload('.ct_tax_media_button.button');
-     $('body').on('click','.ct_tax_media_remove',function(){
-       $('#category-image-id').val('');
-       $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-     });
-     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
-     $(document).ajaxComplete(function(event, xhr, settings) {
-       var queryStringArr = settings.data.split('&');
-       if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
-         var xml = xhr.responseXML;
-         $response = $(xml).find('term_id').text();
-         if($response!=""){
-           // Clear the thumb image
-           $('#category-image-wrapper').html('');
-         }
-       }
-     });
-   });
- </script>
- <?php }
-
   }
 
 $CT_TAX_META_STYLE = new CT_TAX_META_STYLE();
@@ -796,7 +698,7 @@ class CT_TAX_META_DOORSTYLE {
    add_action( 'created_door-style', array ( $this, 'save_doorstyle_image' ), 10, 2 );
    add_action( 'door-style_edit_form_fields', array ( $this, 'update_doorstyle_image' ), 10, 2 );
    add_action( 'edited_door-style', array ( $this, 'updated_doorstyle_image' ), 10, 2 );
-   add_action( 'admin_footer', array ( $this, 'add_script' ) );
+   //add_action( 'admin_footer', array ( $this, 'add_script' ) );
  }
 
  /*
@@ -872,46 +774,7 @@ class CT_TAX_META_DOORSTYLE {
  */
  public function add_script() { ?>
    <script>
-     jQuery(document).ready( function($) {
-       function ct_media_upload(button_class) {
-         var _custom_media = true,
-         _orig_send_attachment = wp.media.editor.send.attachment;
-         $('body').on('click', button_class, function(e) {
-           var button_id = '#'+$(this).attr('id');
-           var send_attachment_bkp = wp.media.editor.send.attachment;
-           var button = $(button_id);
-           _custom_media = true;
-           wp.media.editor.send.attachment = function(props, attachment){
-             if ( _custom_media ) {
-               $('#category-image-id').val(attachment.id);
-               $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-               $('#category-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
-             } else {
-               return _orig_send_attachment.apply( button_id, [props, attachment] );
-             }
-            }
-         wp.media.editor.open(button);
-         return false;
-       });
-     }
-     ct_media_upload('.ct_tax_media_button.button');
-     $('body').on('click','.ct_tax_media_remove',function(){
-       $('#category-image-id').val('');
-       $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-     });
-     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
-     $(document).ajaxComplete(function(event, xhr, settings) {
-       var queryStringArr = settings.data.split('&');
-       if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
-         var xml = xhr.responseXML;
-         $response = $(xml).find('term_id').text();
-         if($response!=""){
-           // Clear the thumb image
-           $('#category-image-wrapper').html('');
-         }
-       }
-     });
-   });
+
  </script>
  <?php }
 
@@ -946,7 +809,7 @@ class CT_TAX_META_PRODUCT {
    add_action( 'created_product-line', array ( $this, 'save_productline_image' ), 10, 2 );
    add_action( 'product-line_edit_form_fields', array ( $this, 'update_productline_image' ), 10, 2 );
    add_action( 'edited_product-line', array ( $this, 'updated_productline_image' ), 10, 2 );
-   add_action( 'admin_footer', array ( $this, 'add_script' ) );
+   //add_action( 'admin_footer', array ( $this, 'add_script' ) );
  }
 
  /*
@@ -1016,55 +879,6 @@ class CT_TAX_META_PRODUCT {
    }
  }
 
-/*
- * Add script
- * @since 1.0.0
- */
- public function add_script() { ?>
-   <script>
-     jQuery(document).ready( function($) {
-       function ct_media_upload(button_class) {
-         var _custom_media = true,
-         _orig_send_attachment = wp.media.editor.send.attachment;
-         $('body').on('click', button_class, function(e) {
-           var button_id = '#'+$(this).attr('id');
-           var send_attachment_bkp = wp.media.editor.send.attachment;
-           var button = $(button_id);
-           _custom_media = true;
-           wp.media.editor.send.attachment = function(props, attachment){
-             if ( _custom_media ) {
-               $('#category-image-id').val(attachment.id);
-               $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-               $('#category-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
-             } else {
-               return _orig_send_attachment.apply( button_id, [props, attachment] );
-             }
-            }
-         wp.media.editor.open(button);
-         return false;
-       });
-     }
-     ct_media_upload('.ct_tax_media_button.button');
-     $('body').on('click','.ct_tax_media_remove',function(){
-       $('#category-image-id').val('');
-       $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-     });
-     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
-     $(document).ajaxComplete(function(event, xhr, settings) {
-       var queryStringArr = settings.data.split('&');
-       if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
-         var xml = xhr.responseXML;
-         $response = $(xml).find('term_id').text();
-         if($response!=""){
-           // Clear the thumb image
-           $('#category-image-wrapper').html('');
-         }
-       }
-     });
-   });
- </script>
- <?php }
-
   }
 
 $CT_TAX_META_PRODUCT = new CT_TAX_META_PRODUCT();
@@ -1096,7 +910,7 @@ class CT_TAX_META_SERIES {
    add_action( 'created_series', array ( $this, 'save_series_image' ), 10, 2 );
    add_action( 'series_edit_form_fields', array ( $this, 'update_series_image' ), 10, 2 );
    add_action( 'edited_series', array ( $this, 'updated_series_image' ), 10, 2 );
-   add_action( 'admin_footer', array ( $this, 'add_script' ) );
+   //add_action( 'admin_footer', array ( $this, 'add_script' ) );
  }
 
  /*
@@ -1166,54 +980,6 @@ class CT_TAX_META_SERIES {
    }
  }
 
-/*
- * Add script
- * @since 1.0.0
- */
- public function add_script() { ?>
-   <script>
-     jQuery(document).ready( function($) {
-       function ct_media_upload(button_class) {
-         var _custom_media = true,
-         _orig_send_attachment = wp.media.editor.send.attachment;
-         $('body').on('click', button_class, function(e) {
-           var button_id = '#'+$(this).attr('id');
-           var send_attachment_bkp = wp.media.editor.send.attachment;
-           var button = $(button_id);
-           _custom_media = true;
-           wp.media.editor.send.attachment = function(props, attachment){
-             if ( _custom_media ) {
-               $('#category-image-id').val(attachment.id);
-               $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-               $('#category-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
-             } else {
-               return _orig_send_attachment.apply( button_id, [props, attachment] );
-             }
-            }
-         wp.media.editor.open(button);
-         return false;
-       });
-     }
-     ct_media_upload('.ct_tax_media_button.button');
-     $('body').on('click','.ct_tax_media_remove',function(){
-       $('#category-image-id').val('');
-       $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-     });
-     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
-     $(document).ajaxComplete(function(event, xhr, settings) {
-       var queryStringArr = settings.data.split('&');
-       if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
-         var xml = xhr.responseXML;
-         $response = $(xml).find('term_id').text();
-         if($response!=""){
-           // Clear the thumb image
-           $('#category-image-wrapper').html('');
-         }
-       }
-     });
-   });
- </script>
- <?php }
 
   }
 
@@ -1241,5 +1007,13 @@ function posts_clauses_with_tax( $clauses, $wp_query ) {
 	return $clauses;
 }
 
+// Add the script required for category images to the applicable admin pages (was interfering with rest of admin)
+function add_admin_scripts( $hook ) {
+	$screen = get_current_screen();
+  if ( $screen->taxonomy == 'frame-material' || 'window-style' || 'door-style' || 'product-line' || 'series' ) {
+    wp_enqueue_script(  'category-images-script', get_bloginfo('template_directory').'/assets/js/category-images.js' );
+  }
+}
+add_action('admin_enqueue_scripts','add_admin_scripts',10,1);
 
 ?>
