858f0ebefba980d4420c9a3ee27381877bbe165c
John Yoder <johngilesyoder@gmail.com>
2017-07-25 12:16:44 -0600

    Remove product type name from additional materials in sidebar

--- wp-content/themes/glo/inc/sidebar-blocks/additional-materials.php
+++ wp-content/themes/glo/inc/sidebar-blocks/additional-materials.php
@@ -20,13 +20,7 @@
         <div class="col-md-6">
           <a href="<?php echo esc_url( get_term_link( $term, $term->taxonomy ) ) ?>" class="single-material">
             <?php echo wp_get_attachment_image( $term->image_id ) ?>
-
-            <?php if (has_term( 'windows', 'product-line' )) : ?>
-              <span class="material-title"><?php echo $term->name; ?> Windows</span>
-            <?php else : ?>
-              <span class="material-title"><?php echo $term->name; ?> Doors</span>
-            <?php endif; ?>
-
+            <span class="material-title"><?php echo $term->name; ?></span>
           </a>
         </div>
       <?php } ?>
