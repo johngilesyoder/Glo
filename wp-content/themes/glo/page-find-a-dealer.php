ff4083af9de9423000312c930e8887765e9c3a7b
John Yoder <johngilesyoder@gmail.com>
2020-03-17 10:26:39 -0600

    Fix feature image

--- wp-content/themes/glo/page-find-a-dealer.php
+++ wp-content/themes/glo/page-find-a-dealer.php
@@ -9,14 +9,36 @@ $about_us_photo = get_field('about_us_photo');
 
 get_header(); ?>
 
-<section class="hero-title">
-  <div class="hero-bg" style="background-image:url('<?php echo $hero_background['url']; ?>')"></div>
-  <div class="hero-content">
-    <div>
-      <h1><?php the_title(); ?></h1>
-    </div>
-  </div>
-</section>
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php $post_id = get_the_ID(); ?>
+
+	<?php if ( has_post_thumbnail() ) : ?>
+
+		<?php $thumb = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+
+		<section class="hero-title">
+		  <div class="hero-bg" style="background-image:url('<?php echo $thumb; ?>')"></div>
+		  <div class="hero-content">
+		    <h1><?php the_title(); ?></h1>
+		  </div>
+		</section>
+
+	<?php else : ?>
+
+		<div class="page-title">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<?php endif; ?>
+  
+<?php endwhile; ?>
 
 <main class="page-wrapper">
   <!-- Map -->
